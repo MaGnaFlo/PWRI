ee66f4ac0b91605c7ccfea408918a593d24807c7
victorpetitjean <victor.petitjean@institutoptique.fr>
2015-11-24 13:25:13 +0100

    Push Victor

--- Sources/operation.cpp
+++ Sources/operation.cpp
@@ -1,9 +1,33 @@
+/*!
+ * \file operation.cpp
+ * \brief Classe d'opérations générales.
+ * \author {F. Lardeux, T. Maifret, V. Petitjean}
+ * \date 20/11/2015
+ *
+ * Classe comprenant plusieurs opérations basiques, telles que des modifications de matrices, de chaînes de caractres ...
+ */
+
+
 #include "operation.h"
 #include <iostream>
+
+/*!
+ * \fn Operation::Operation()
+ * \brief Constructeur
+ */
 
 Operation::Operation()
 {
 }
+
+/*!
+ * \fn std::string Operation::int2string(int n)
+ * \brief Int vers String
+ *
+ * \param n : entier à transformer en string
+ *
+ * Renvoie une chaine de 3 caractères correspondant à l'entier n (ex : 12 -> "012")
+ */
 
 std::string Operation::int2string(int n)
 {
@@ -20,6 +44,16 @@ std::string Operation::int2string(int n)
     return number;
 }
 
+/*!
+ * \fn cv::Mat Operation::convert2x3to3x3(cv::Mat transf2x3)
+ * \brief Matrice 2x3 en matrice 3x3
+ *
+ * \param transf2x3 : matice de transformation 2x3
+ * \return matrice de transformation homogène 3x3
+ *
+ * Ajout d'une ligne [0 0 1] pour rendre la matrice de transfo 2x3 homogène
+ */
+
 cv::Mat Operation::convert2x3to3x3(cv::Mat transf2x3)
 {
     cv::Mat transf3x3 = cv::Mat::zeros(3,3,CV_64F);
@@ -31,6 +65,14 @@ cv::Mat Operation::convert2x3to3x3(cv::Mat transf2x3)
 
     return transf3x3;
 }
+
+/*!
+ * \fn cv::Mat Operation::meanMat(const std::vector<cv::Mat> matrices)
+ * \brief Moyenne d'un vecteur de Matrices
+ *
+ * \param matrices : vecteur de Matrices nxm
+ * \return une matrice mxn, moyenne des matrices du vecteur paramètre
+ */
 
 cv::Mat Operation::meanMat(const std::vector<cv::Mat> matrices)
 {
