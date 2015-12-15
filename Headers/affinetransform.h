567be89a6a98bae300719ffb0a4f28a35b5bc68c
victorpetitjean <victor.petitjean@institutoptique.fr>
2015-12-15 11:13:47 +0100

    Comments - 20151215
    Comments added/updated in
    - affinetransform.h
    - operation.h
    - videomanager.h

--- Headers/affinetransform.h
+++ Headers/affinetransform.h
@@ -1,3 +1,12 @@
+/*!
+ * Authors : Florian Lardeux, Thomas Maifret, Victor Petitjean
+ * November 2015
+ *
+ * affineTransform is a complete class that calculates the affine transformation between images
+ * using feature detection & feature matching. It also saves the undistorted images.
+ * It relies on the FeatureDetector class to detect features with OpenCV tools
+*/
+
 #ifndef AFFINETRANSFORM_H
 #define AFFINETRANSFORM_H
 
@@ -18,15 +27,51 @@ class affineTransform
 public:
     //Constructors
     affineTransform();
+	
+	/*!
+	 * This constructor sets all the images that ar going to be processed.
+	 * The images go from [path + name + imgStart + ext] to [path + name + imgEnd + ext]
+	*/
     affineTransform(int imgStart, int imgEnd, std::string path, std::string name, std::string ext);
 
     //Methods
+	
+	/*!
+	 * setMidXY sets the middle of the images (height and width over 2)
+	*/
     void setMidXY();
+	
+	/*!
+	 * UNUSED createWarpedImages_rec
+	 * UNUSED createWarpedImages_it()
+	 * createWarpedImages_it(const std::string &pathSave, QProgressBar *progressBar)
+	 * These 3 methods save the processed (aka undistorded) images, either the process being recursive or iterative
+	 * createWarpedImages_it takes a saving path. The processed images have the name [pathSave + "Processed_" + name + number + ext]
+	 * The methods also takes a pointer to a QProgressBar, to update it while processing
+	*/
     cv::Mat createWarpedImages_rec(const int &i);
     void createWarpedImages_it();
     void createWarpedImages_it(const std::string &pathSave, QProgressBar *progressBar);
+	
+	/*!
+	 * findAffineTransform calculates the transform matrix between two (successive) images
+	 * Feature matches (pairs of keypoints) have been previously calculated, and the methods selects the best pairs
+	 * to calculate the best transformation matrix
+	*/
     void findAffineTransform(const std::vector<cv::DMatch> &matches, const std::vector<cv::KeyPoint> &keypoint_1, const std::vector<cv::KeyPoint> &keypoint_2);
-    void process(QProgressBar *progressBar);
+    
+	/*!
+	 * process is the method to call to calculate the mean transformation matrix of an entire set.
+	 * It calls findAffineTransform for each pair of images, after having found the keypoints and matches.
+	 * Then it returns the mean transformation matrix
+	*/
+	void process(QProgressBar *progressBar);
+	
+	/*!
+	 * displayMatches
+	 * displayFeatures
+	 * These 2 methods display the keypoints and/or matches, to verify the results returned by the OpenCV tools
+	*/
     void displayMatches(const std::vector<cv::DMatch> &matches, const std::vector<cv::KeyPoint> &keypoint_1, const std::vector<cv::KeyPoint> &keypoint_2, const cv::Mat &img1, const cv::Mat &img2, const std::string path);
     void displayFeatures(const std::vector<cv::KeyPoint> &kpt1, const std::vector<cv::KeyPoint> &kpt2, int i);
 
--- Headers/videomanager.h
+++ Headers/videomanager.h
@@ -1,4 +1,4 @@
-/*
+/*!
  * Author : Florian Lardeux - November 2015
  * videoManager is a simple, easily alterable class that allows the management of videos
  * and images.
@@ -20,14 +20,14 @@ public:
     virtual void loadFile();
     virtual void saveFile();
 
-    /* Load a set of images via their path, their extension, the name of the images (if there is one)
+    /*! videoManager loads a set of images via their path, their extension, the name of the images (if there is one)
      * and the name given to the video, if you want to create one.
      * Basically the name of an image must be : image_name 001.png for instance.
      * If the images are just "001.png", don't bother put a name, same thing for the video name.
      */
     videoManager(std::string path, std::string extension, int n_images, std::string name = "", std::string videoName = "");
 
-    /* This method allows to create a video out of the image sequence you named in the constructor or
+    /*! This method allows to create a video out of the image sequence you named in the constructor or
      * using the setters.
      */
     void setVideo();
